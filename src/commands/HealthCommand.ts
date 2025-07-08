4d89d3fdab935bb4ea2efeceab8af62373bf7060
ryk_cbaool <136170031+rykcbaool@users.noreply.github.com>
2025-07-08 19:39:44 -0400

    Create HealthCommand.ts

---
+++ src/commands/HealthCommand.ts
@@ -0,0 +1,14 @@
+export class HealthCommand implements CommandHandler {
+  private readonly bot: Bot
+  readonly cmdName = 'health'
+
+  constructor(bot: Bot) {
+    this.bot = bot
+  }
+
+  execute(args: string[]): CommandExecution | null {
+    const health = this.bot.health
+    this.bot.terminal?.log(`Health: ${health} hearts`)
+    return null
+  }
+}
