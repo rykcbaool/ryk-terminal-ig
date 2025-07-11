e096e1e7c65e984b6c433ff2c7eb4a75d1609b25
rykcbaoolNEW <ryker.8811@gmail.com>
2025-07-10 20:44:59 -0400

    commit8

--- lib/commands/HealthCommand.d.ts
+++
@@ -1,8 +0,0 @@
-import { Bot } from 'mineflayer';
-import { CommandHandler, CommandExecution } from '../Commands';
-export declare class HealthCommand implements CommandHandler {
-    private readonly bot;
-    readonly cmdName = "health";
-    constructor(bot: Bot);
-    execute(args: string[], number: number): void | CommandExecution | Promise<void | CommandExecution | null> | null;
-}
--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Bot } from 'mineflayer';
-import { UserInterface } from './UserInterface';
-declare module 'mineflayer' {
-    interface Bot {
-        terminal: UserInterface;
-    }
-}
-export declare function plugin(bot: Bot): void;
